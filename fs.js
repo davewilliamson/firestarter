de1d1d638a51b734191fb494f8b020c9420853b3
Dave Williamson <dave@ukit.net>
2013-01-16 00:03:06 +0100

    Refactor of main fs.js

--- fs.js
+++ fs.js
@@ -1,66 +1,30 @@
 "use strict";
 
-require('./setLogging')({
-	monoOnly : false,
-	logLevel : 10
+var logLevel = 3, masterArgs = new Array(process.argv.length);
+
+process.argv.forEach(function(val, index, array) {
+	if (index > 2)
+		masterArgs[index] = val;
+	if (index == 2)
+		logLevel = val;
 });
 
-var numCPUs = require('os').cpus().length;
+console.log("Log level set to: "+logLevel);
 
-var workers = new Array(numCPUs);
+require('./setLogging')({
+	monoOnly : false,
+	logLevel : logLevel
+});
 
-var cp = require('child_process');
-var uuid = require('node-uuid');
-var masterArgs = new Array(20);
+var numCPUs = require('os').cpus().length
+  , workers = new Array(numCPUs)
+  , cp = require('child_process')
+  , uuid = require('node-uuid');
 
 console.inspect(process.versions);
 console.inspect(process.config);
 console.inspect(process.memoryUsage());
 console.log('This process is pid ' + process.pid + ':' + process.title);
-
-process.on('uncaughtException', function(err) {
-	console.error('Caught exception: ' + err);
-	err = null;
-	process.exit(1);
-});
-
-process.argv.forEach(function(val, index, array) {
-	console.log(index + ': ' + val);
-	if (index > 2)
-		masterArgs[index] = val;
-});
-
-var workerListen = function(m) {
-
-	console.debug('Child got exception: ' + m + ', from child #:' + m.process);
-
-	if (m.message == "exception") {
-
-		workers[m.process].kill('SIGHUP');
-		var id = '' + uuid.v4();
-
-		workers[id] = cp.fork('./testLogging', [
-				'worker=' + id, masterArgs
-		]);
-		workers[id].on('message', workerListen);
-
-		delete workers[m.process];
-
-		m = null;
-		id = null;
-
-	}
-	m = null;
-};
-
-process.on('SIGINT', function() {
-
-	workers.forEach(function(worker) {
-		worker.kill('SIGHUP');
-		worker = null;
-	});
-	process.exit(0);
-});
 
 try {
 	process.setgid('nogroup');
@@ -80,6 +44,44 @@ try {
 	err = null;
 	throw new Error("Could not switch to low(/no) priority group");
 }
+
+var workerListen = function(m) {
+
+	console.debug('Child got exception: ' + m + ', from child #:' + m.process);
+
+	if (m.message == "exception") {
+
+		workers[m.process].kill('SIGHUP');
+		
+		var id = '' + uuid.v4();
+
+		workers[id] = cp.fork('./testLogging', [
+				'worker=' + id, masterArgs
+		]);
+		workers[id].on('message', workerListen);
+
+		delete workers[m.process];
+
+		m = null;
+		id = null;
+
+	}
+	m = null;
+};
+
+process.on('uncaughtException', function(err) {
+	console.error('Caught exception: ' + err);
+	err = null;
+	process.exit(1);
+});
+
+process.on('SIGINT', function() {
+	workers.forEach(function(worker) {
+		worker.kill('SIGHUP');
+		worker = null;
+	});
+	process.exit(0);
+});
 
 exports.run = function(fileName){
 	
